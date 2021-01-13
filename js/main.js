1ea5165bab265daeeb84bb615a939f4c28dc268b
Logan Brock <36110596+lgbrock@users.noreply.github.com>
2021-01-13 17:57:07 -0600

    last update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');//skip if it is not a transform
+}
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return; //stop the function from running all together
+
+    key.classList.add('playing');
     audio.currentTime = 0; //rewind to the start
     audio.play();
-    key.classList.add('playing');
 }
 
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return; //skip if it is not a transform
-    this.classList.remove('playing');
-
-}
-
-const keys = document.querySelectorAll('.key');
+const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
